241d8e25bd19504e2c46c0d9412b4dbd8f05e7d1
tscallioglu <60489336+tscallioglu@users.noreply.github.com>
2020-12-12 07:42:04 +0300

    Add files via upload

---
+++ Happy or Sad Classifier.py
@@ -0,0 +1,60 @@
+import os
+import zipfile
+
+import tensorflow as tf
+
+import wget
+
+DESIRED_ACCURACY = 0.999
+
+wget.download( "https://storage.googleapis.com/laurencemoroney-blog.appspot.com/happy-or-sad.zip", "/tmp/happy-or-sad.zip")
+
+zip_ref = zipfile.ZipFile("/tmp/happy-or-sad.zip", 'r')
+zip_ref.extractall("/tmp/h-or-s")
+zip_ref.close()
+
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('acc')>DESIRED_ACCURACY):
+      print("\nReached 99.9% accuracy so cancelling training!")
+      self.model.stop_training = True
+
+callbacks = myCallback()
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+
+from tensorflow.python.keras.optimizers import RMSprop
+
+model.compile(loss='binary_crossentropy',
+              optimizer=RMSprop(lr=0.001),
+              metrics=['acc'])
+
+
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale=1/255)
+
+train_generator = train_datagen.flow_from_directory(
+        "/tmp/h-or-s",  
+        target_size=(150, 150), 
+        batch_size=10,
+        class_mode='binary')
+
+
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=2,  
+      epochs=15,
+      verbose=1,
+      callbacks=[callbacks])
